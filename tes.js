dfedbfea19a89dd8a802fe429973c21f48e939a6
paulosarinho <102939636+paulosarinho@users.noreply.github.com>
2024-03-12 15:00:50 -0300

    sad

---
+++ tes.js
@@ -0,0 +1,8 @@
+function aceitar() {
+    alert("Ótimo! Mal posso esperar para passar um tempo juntos.");
+}
+
+function recusar() {
+    var errorMsg = document.getElementById('erro-msg');
+    errorMsg.style.display = 'block';
+}
